14920915706fd345d7cc80378f431c5af342702d
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-01-09 14:43:21 +0100

    Creata una nuova cartella Data contenente il file db.php in cui andranno tutti gli oggetti creati dalla classe Movie

---
+++ data/db.php
@@ -0,0 +1,13 @@
+<?php // All objects deriving from the Movie class will go here
+
+    $back_to_the_future = new Movie(
+        'Back to the Future', // Title
+        ['Fantasy', 'Comedy', 'Romance', 'Adventure', 'Science fiction'], // Genres
+        4 // Vote review
+    );
+
+    $in_time = new Movie(
+        'In Time', // Title
+        ['Action', 'Drama', 'Crime fiction', 'Thriller', 'Science fiction', 'Melodrama'], // Genres
+        5 // Vote review
+    );
--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php 
-// create un file index.php in cui:
-// - è definita una classe ‘Movie’
-//   => all'interno della classe sono dichiarate delle variabili d'istanza
-//   => all'interno della classe è definito un costruttore
-//   => all'interno della classe è definito almeno un metodo
-// - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
-// Bonus 1:
-// Modificare la classe Movie in modo che accetti piú di un genere.
 // Bonus 2:
 // Creare un layout completo per stampare a schermo una lista di movies.
 // Facciamo attenzione all’organizzazione del codice, suddividendolo in appositi file e cartelle. Possiamo ad esempio organizzare il codice
@@ -15,18 +7,7 @@
 // - organizzando il layout dividendo la struttura ed i contenuti in file e parziali dedicati.
     
     require_once __DIR__ . '/classes/Movie.php';
-
-    $back_to_the_future = new Movie(
-        'Back to the Future', // Title
-        ['Fantasy', 'Comedy', 'Romance', 'Adventure', 'Science fiction'], // Genres
-        4 // Vote review
-    );
-
-    $in_time = new Movie(
-        'In Time', // Title
-        ['Action', 'Drama', 'Crime fiction', 'Thriller', 'Science fiction', 'Melodrama'], // Genres
-        5 // Vote review
-    );
+    require_once __DIR__ . '/data/db.php';
 
     var_dump($back_to_the_future, $in_time);
 ?>
